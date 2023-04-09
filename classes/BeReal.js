cb1de4a6d1595b6dab6d2cd815cfd9af52c85711
dark <bentalesnik@gmail.com>
2023-04-09 18:31:14 -0400

    make it better

--- classes/BeReal.js
+++ classes/BeReal.js
@@ -1,10 +1,88 @@
 const request = require("request-promise");
+const signale = require("signale");
+signale.config({
+  displayTimestamp: true,
+  displayDate: true,
+  displayFilename: true,
+});
 
 module.exports = {
   login: async (phoneNumber) => {
-    // TODO: Add login with console inputs and save userToken to a file so it doesn't have to be entered every time and the bearer token can be used to get the user's memories
+    const options = {
+      method: "POST",
+      uri: "https://us-central1-befake-623af.cloudfunctions.net/login",
+      body: `{"phoneNumber":"${phoneNumber}"}`,
+      // headers: {
+      //   accept: "*/*",
+      //   "accept-language": "en-US,en;q=0.9",
+      //   "content-type": "text/plain;charset=UTF-8",
+      //   "sec-ch-ua":
+      //     '"Google Chrome";v="111", "Not(A:Brand";v="8", "Chromium";v="111"',
+      //   "sec-ch-ua-mobile": "?0",
+      //   "sec-ch-ua-platform": '"macOS"',
+      //   "sec-fetch-dest": "empty",
+      //   "sec-fetch-mode": "cors",
+      //   "sec-fetch-site": "cross-site",
+      // },
+    };
+
+    const response = await request(options);
+
+    if (JSON.parse(response).error) {
+      signale.error("Too many requests, please try again later.");
+      process.exit(1);
+    }
+    // call the verify function in this file
+    const sessionInfo = JSON.parse(response).sessionInfo;
+    return sessionInfo;
   },
-  getMemories: async (userToken) => {
+  verify: async (sessionInfo, code) => {
+    const options = {
+      method: "POST",
+      uri: "https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPhoneNumber?key=AIzaSyDwjfEeparokD7sXPVQli9NsTuhT6fJ6iA",
+      body: `{"sessionInfo":"${sessionInfo}","code":"${code}","operation":"SIGN_UP_OR_IN"}`,
+    };
+
+    try {
+      const response = await request(options);
+      const userToken = JSON.parse(response).idToken;
+      const refreshToken = JSON.parse(response).refreshToken;
+
+      return [userToken, refreshToken];
+    } catch (error) {
+      return false;
+    }
+  },
+  refresh: async (refreshToken) => {
+    signale.info("Refreshing token...");
+    const options = {
+      method: "POST",
+      uri: "https://securetoken.googleapis.com/v1/token?key=AIzaSyDwjfEeparokD7sXPVQli9NsTuhT6fJ6iA",
+      body: `{"grant_type":"refresh_token","refresh_token":"${refreshToken}"}`,
+    };
+
+    try {
+      const response = await request(options);
+      const userToken = JSON.parse(response).id_token;
+      signale.success("Token refreshed!");
+
+      const config = require("../config.json");
+      config.BEREAL.USER_TOKEN = userToken;
+
+      const fs = require("fs");
+      fs.writeFileSync(
+        "../config.json",
+        JSON.stringify(config, null, 2),
+        "utf8"
+      );
+
+      return userToken;
+    } catch (error) {
+      signale.error("Error while refreshing token. Ending process...");
+      process.exit(1);
+    }
+  },
+  getMemories: async (userToken, refreshToken) => {
     const options = {
       method: "GET",
       uri: "https://mobile.bereal.com/api/feeds/memories",
@@ -30,9 +108,18 @@ module.exports = {
         });
       }
 
-      return filteredMemories;
+      return {
+        memories: filteredMemories,
+        userToken: userToken,
+        refreshToken: refreshToken,
+      };
     } catch (error) {
-      console.log(error);
+      if (error.statusCode == 403) {
+        const userToken = await module.exports.refresh(refreshToken);
+        return module.exports.getMemories(userToken, refreshToken);
+      } else {
+        return false;
+      }
     }
   },
 };
--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-require("dotenv/config");
-
 const signale = require("signale");
 signale.config({
   displayTimestamp: true,
@@ -7,17 +5,13 @@ signale.config({
   displayFilename: true,
 });
 
-const {
-  USER_TOKEN = userToken,
-  IFTTT_URL = iftttURL,
-  IMGUR_CLIENT_ID = clientID,
-  IMGUR_CLIENT_SECRET = clientSecret,
-  IMGUR_REFRESH_TOKEN = refreshToken,
-} = process.env;
+const fs = require("fs");
+const config = require("./config.json");
 
 const BeReal = require("./classes/BeReal.js");
 const Notification = require("./classes/Notification.js");
 const Merge = require("./classes/Merge.js");
+const input = require("input");
 
 // get current date for EST timezone
 const date = new Date();
@@ -26,14 +20,55 @@ date.setHours(date.getHours() - 4);
 // get date in format YYYY-MM-DD, including leading zeros
 const today = date.toISOString().slice(0, 10);
 
+async function updateConfig(token, refreshToken) {
+  config.BEREAL.USER_TOKEN = token;
+  config.BEREAL.REFRESH_TOKEN = refreshToken;
+
+  fs.writeFile("./config.json", JSON.stringify(config), (err) => {
+    if (err) console.log(err);
+  });
+}
+
 const main = async () => {
   signale.start("Starting script");
+
+  if (config.BEREAL.USER_TOKEN === "" || config.BEREAL.REFRESH_TOKEN === "") {
+    signale.info("No user token found, logging in");
+
+    const phone = await input.text(
+      "Enter your phone number with country code (ex. +14439002423): "
+    );
+    const sessionInfo = await BeReal.login(phone);
+
+    let verified = false;
+    while (!verified) {
+      const code = await input.text("Enter the code you received: ");
+
+      const req = await BeReal.verify(sessionInfo, code);
+      if (!req) {
+        signale.error("Invalid code, please try again");
+        continue;
+      }
+
+      await updateConfig(req[0], req[1]);
+
+      verified = true;
+      signale.success("Logged in successfully");
+    }
+  } else {
+    signale.info("User token found, skipping login");
+  }
+
   signale.info("Fetching memories");
 
-  // wait for memories to return
-  const memories = await BeReal.getMemories(USER_TOKEN);
+  const memories = await BeReal.getMemories(
+    config.BEREAL.USER_TOKEN,
+    config.BEREAL.REFRESH_TOKEN
+  );
 
-  for (const memory of memories) {
+  await updateConfig(memories.userToken, memories.refreshToken);
+
+  for (const memory of memories.memories) {
     if (memory.date.slice(5, 10) === today.slice(5, 10)) {
       if (memory.date.slice(0, 4) !== today.slice(0, 4)) {
         signale.info("Found memory from today");
@@ -41,12 +76,11 @@ const main = async () => {
         const url = await Merge.start(
           memory.primary,
           memory.secondary,
-          IMGUR_CLIENT_ID,
-          IMGUR_CLIENT_SECRET,
-          IMGUR_REFRESH_TOKEN
+          config.IMGUR.CLIENT_ID,
+          config.IMGUR.CLIENT_SECRET,
+          config.IMGUR.REFRESH_TOKEN
         );
 
-        // date in format Friday, January 1, 2021
         const date = new Date(memory.date).toDateString();
 
         signale.info("Sending notification");
@@ -54,12 +88,19 @@ const main = async () => {
           `⚠️ BeReal, One Year Ago Today ⚠️`,
           `Here's your BeReal memory from ${date}!`,
           url,
-          IFTTT_URL
+          config.IFTTT.URL
         );
+        signale.success("Notification sent! Waiting until tomorrow!");
       }
     }
   }
-  // print return
 };
 
-main();
+const schedule = require("node-schedule");
+let rule = new schedule.RecurrenceRule();
+
+rule.tz = "America/New_York";
+rule.hour = 8;
+rule.minute = 0;
+
+schedule.scheduleJob(rule, main);
